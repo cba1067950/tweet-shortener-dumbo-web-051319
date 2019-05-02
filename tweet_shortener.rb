30424a292407e8d538bc714b2dc2731bc005df2e
Joe Jamsky <cba1067950@yahoo.com>
2019-05-02 03:14:26 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def dictionary
+  tweet_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  hash = dictionary
+  keys = dictionary.keys
+  tweet_array = tweet.split
+
+  tweet_array.each do |word|
+    keys.each do |compare|
+      if word == compare
+        tweet = tweet.gsub(/\b#{word}\b/, hash[word])
+      end
+    end
+  end
+  tweet
+end
+
